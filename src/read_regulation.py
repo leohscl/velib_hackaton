15d9634aad9ea011e8c68c06ddb1ca3f8725b9b2
leo <leo.henches@gmail.com>
2023-10-02 16:39:39 +0200

    Added count of regulation operations (velib added - velib taken) by district

--- src/read_regulation.py
+++ src/read_regulation.py
@@ -4,12 +4,20 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 import contextily as ctx
 from shapely.geometry import Point, LineString
+import glob
 
 # doc: ../data/raw/14-Hackathon/03_R├йgulation_mouvements_stations/Descriptif_r├йgulation_mouvements_stations.txt
-file = "../data/raw/14-Hackathon/03_R├йgulation_mouvements_stations/2023_02/2023-02-01_Mouvements_de_r├йgulation.csv"
-regulation = pd.read_csv(file)
-regulation.head()
-regulation.columns
+# file = "../data/raw/14-Hackathon/03_R├йgulation_mouvements_stations/2023_02/2023-02-01_Mouvements_de_r├йgulation.csv"
+# regulation = pd.read_csv(file)
+files = glob.glob("../data/raw/14-Hackathon/03_R├йgulation_mouvements_stations/2023_02/*.csv")
+files.sort()
+
+regulation = pd.DataFrame()
+list_df = [pd.read_csv(file) for file in files]
+for i, df in enumerate(list_df):
+    df["day"] = i + 1
+
+regulation = pd.concat(list_df)
 
 regulation["Latitude station prise"] = regulation["Latitude station prise"].str.replace(',', '.').astype(float)
 regulation["Longitude station prise"] = regulation["Longitude station prise"].str.replace(',', '.').astype(float)
@@ -26,18 +34,53 @@ regulation_line = gpd.GeoDataFrame(regulation, crs=4326, geometry=geometry_line)
 regulation_start = regulation_start.to_crs(epsg = 3857)
 regulation_end = regulation_end.to_crs(epsg = 3857)
 regulation_line = regulation_line.to_crs(epsg = 3857)
+
 districts = gpd.read_file("../data/raw/quartier_paris.geojson").to_crs(epsg=3857)
+departements = gpd.read_file("../data/raw/departements.geojson").to_crs(epsg=3857)
+
+departements_idf_no_paris = departements[departements["code"].isin(["92", "93", "94", "78", "91", "77", "95"])]
+departements_petite_couronne_no_paris = departements[departements["code"].isin(["92", "93", "94"])]
+
+# departements_idf_no_paris.to_file("../data/raw/departements_idf_no_paris.geojson", driver='GeoJSON')
+# departements_petite_couronne_no_paris.to_file("../data/raw/departements_petite_couronne_no_paris.geojson", driver='GeoJSON')
+all_districts = pd.concat([districts, departements_petite_couronne_no_paris])
+
+
+# plot all operations for first day
+regulation_day_1_start = regulation_start[regulation_start["day"] == 1]
+regulation_day_1_end = regulation_end[regulation_end["day"] == 1]
+regulation_day_1_line = regulation_line[regulation_line["day"] == 1]
 
 fig, ax = plt.subplots()
-districts.plot(ax=ax, alpha=0.5, edgecolor="white")
+all_districts.plot(ax=ax, alpha=0.5, edgecolor="white")
 # plot points and lines
-regulation_start.plot(ax=ax, color = "green")
-regulation_end.plot(ax=ax, color = "red")
-regulation_line.plot(ax=ax, color = "lightblue")
+regulation_day_1_start.plot(ax=ax, color = "green")
+regulation_day_1_end.plot(ax=ax, color = "red")
+regulation_day_1_line.plot(ax=ax, color = "lightblue")
 ax.set_axis_off()
 ctx.add_basemap(ax)
 
 
 plt.savefig('../img/regulation_first_night.png')
 
-# plt.show()
+plt.show()
+
+
+# velib added - velib taken by district 
+all_districts["velib_added"] = all_districts.apply(
+    lambda district: (
+        (regulation_end.within(district.geometry) * regulation_end["Total"]) - (regulation_start.within(district.geometry) * regulation_start["Total"]) 
+    ).sum(),
+    axis=1
+)
+
+fig, ax = plt.subplots(figsize=(8, 6))
+all_districts.plot("velib_added", cmap="PuOr", ax=ax, alpha=0.5, edgecolor="white", legend=True)
+ax.set_axis_off()
+ctx.add_basemap(ax)
+plt.savefig('../img/all_regulation_february.png')
+plt.show()
+
+# should be 0, somehow is -12, maybe some stations are not in the polygons
+# sum(all_districts["velib_added"])
+
